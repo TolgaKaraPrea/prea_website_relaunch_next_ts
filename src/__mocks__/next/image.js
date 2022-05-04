d4f0c6ee27b57f12a4f77f828f5079b9a47c4ef2
TolgaKaraPrea <tolga.kara@prea.eu>
2022-05-04 11:55:27 +0200

    feat: added commitlint to the package

--- src/__mocks__/next/image.js
+++ src/__mocks__/next/image.js
@@ -1,10 +1,11 @@
-import jest from "jest";
+const jest = require("jest");
+
 let mockImage = jest.genMockFromModule("next/image");
 
 const image = {
   __esModule: true,
   default: () => {
-    return "Next image stub"; // whatever
+    return "Next image stub";
   }
 };
 
